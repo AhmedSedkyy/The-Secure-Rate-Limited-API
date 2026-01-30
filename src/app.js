bc82797164d8ab65665fcf6a5b849b119a2db33c
AhmedSedkyy <ahmedsedkykh@gmail.com>
2026-01-30 09:47:30 +0200

    Add global error handling middleware and utility functions

--- src/app.js
+++ src/app.js
@@ -19,11 +19,30 @@ app.use(express.json());
 
 
 
+/* --------------Global error middleware----------------------*/
 
+app.all("/*splat", (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+})
+
+
+app.use((err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+
+    if (err.isOperational) {
+        res.status(statusCode).json({ success: false, error: err.message });
+    } else {
+
+        console.error('SYSTEM ERROR:', err);
+
+        res.status(500).json({ success: false, error: "Something went wrong! Please try again later." });
+
+    }
+})
 
 
 /*               Start Server                                  */
-
 
 const port =process.env.PORT || 3000
 app.listen(port, () =>  {
